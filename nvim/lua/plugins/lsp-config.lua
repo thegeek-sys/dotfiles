18cf5e9a8bfec1f1780d4ec73e9fd12eca90dc07
Flavio Sperandeo <55345633+thegeek-sys@users.noreply.github.com>
2025-09-24 00:11:47 +0200

    new sketchybar, update nvim

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -7,13 +7,13 @@ return {
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
+		version = "1.28.0",
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = {
 					"lua_ls",
 					"arduino_language_server",
 					"html",
-					"ts_ls",
 					"pyright",
 					"clangd"
 				},
@@ -23,14 +23,24 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.jdtls.setup({})
-			lspconfig.arduino_language_server.setup({})
-			lspconfig.html.setup({})
-			lspconfig.ts_ls.setup({})
-			lspconfig.pyright.setup({})
-			lspconfig.clangd.setup({})
+			vim.lsp.config("lua_ls", {})
+			vim.lsp.config("jdtls", {})
+			vim.lsp.config("arduino_language_server", {})
+			vim.lsp.config("html", {})
+			vim.lsp.config("ts_ls", {})
+			vim.lsp.config("pyright", {})
+			vim.lsp.config("clangd", {})
+
+			-- abilita i server che hai configurato
+			vim.lsp.enable({
+				"lua_ls",
+				"jdtls",
+				"arduino_language_server",
+				"html",
+				"ts_ls",
+				"pyright",
+				"clangd",
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
